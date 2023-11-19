e190fc853e47210573104d0c010b4bc742737df2
Huzaifa Awan <mhuzaifaawan7@gmail.com>
2023-11-19 16:29:45 +0500

    Completed the Functionality of generating code in the backend fixed the dashboard a bit the subject portion is hardcoded for now will work on it in future to make it dynamic for now the attendence system is for our class only baki sab ke abhi khali hain lakin code likh dia he how to access them via code generation

---
+++ Controller/dashboardController.js
@@ -0,0 +1,51 @@
+﻿async function getDashboardData(session, semester, section, subject) {
+
+    const code = getCode(session, semester, section, subject);
+
+    const data = await fetch('/dashboardController.php', {
+        method: 'POST',
+        body: JSON.stringify({ code })
+    });
+
+    return await data.json();
+
+}
+function getCode(session, semester, section, subject) {
+
+    let code = '';
+
+    if (session === 'Morning') {
+        code += '1';
+    } else {
+        code += '2';
+    }
+
+    code += semester;
+
+    if (section === 'A') {
+        code += '1';
+    } else if (section === 'B') {
+        code += '2';
+    }
+
+    if (subject === 'SE') {
+        code += '1';
+    } else if (subject === 'WP') {
+        code += '2';
+    } else if (subject === 'Lab-Web Prog') {
+        code += '3';
+    } else if (subject === 'DM') {
+        code += '4';
+    } else if (subject === 'AOOP') {
+        code += '5';
+    } else if (subject === 'Lab-AOOP') {
+        code += '6';
+    } else if (subject === 'DE') {
+        code += '7';
+    } else if (subject === 'DAA') {
+        code += '8';
+    }
+
+    return code;
+
+}
