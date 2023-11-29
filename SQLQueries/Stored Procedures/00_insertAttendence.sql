de876da8e30b78945ec293d609c7333c4facf2ee
Huzaifa Awan <mhuzaifaawan7@gmail.com>
2023-11-29 10:18:26 +0500

    Updated Query

--- SQLQueries/Stored Procedures/00_insertAttendence.sql
+++ SQLQueries/Stored Procedures/00_insertAttendence.sql
@@ -1,29 +1,29 @@
 DELIMITER $$
-DROP PROCEDURE IF EXISTS  sp_insert_attendance$$
+DROP PROCEDURE IF EXISTS sp_insert_attendance_today$$ 
 DELIMITER $$
-
-CREATE PROCEDURE sp_insert_attendance(
-    IN p_tableName VARCHAR(255), -- Main table name (e.g., student_141)
-    IN p_subjectTableName VARCHAR(255) -- Subject table name (e.g., student_141_AOOP)
+CREATE PROCEDURE sp_insert_attendance_today(
+  IN p_tableName VARCHAR(255),
+  IN p_subjectTableName VARCHAR(255)
 )
 BEGIN
-    -- Declare variables
-    SET @current_date = CURRENT_DATE();
+  DECLARE last_execution_date DATE;
 
-    -- Build dynamic SQL query
-    SET @sql_query = CONCAT('INSERT INTO ', p_subjectTableName, ' (rollNo, DATE, slot1, slot2) ', 
-                            'SELECT rollNo, ?, "P", "P" FROM ', p_tableName);
+  SELECT LastExecutionDate INTO last_execution_date
+  FROM DailyProcedureLog
+  WHERE ProcedureName = 'sp_insert_attendance_daily' AND subjName = p_subjectTableName;
 
-    -- Prepare and execute the dynamic SQL query
-    PREPARE dynamic_sql_query FROM @sql_query;
-    -- Set parameter values, execute, etc.
-    -- Execute the prepared statement with current date
-    EXECUTE dynamic_sql_query USING @current_date;
-
-    -- Deallocate the prepared statement
-    DEALLOCATE PREPARE dynamic_sql_query;
-END $$
-
+  IF (last_execution_date IS NULL OR 
+      DATEDIFF(CURRENT_DATE(), last_execution_date) > 0) THEN
+    
+    CALL sp_insert_attendance(p_tableName, p_subjectTableName);
+    
+    INSERT INTO DailyProcedureLog (ProcedureName,  
+                                   LastExecutionDate,
+                                   subjName) 
+    VALUES ('sp_insert_attendance_daily',
+            CURRENT_DATE(),
+            p_subjectTableName);
+             
+  END IF;
+END$$
 DELIMITER ;
-
-
